cc526b620c9ad0985457d4b3d38140dd68b07c56
Aditya Vaish <adityavaish846@gmail.com>
2025-02-01 15:56:50 +0530

    Spam Pybot repo created

---
+++ spam-pybot.py
@@ -0,0 +1,14 @@
+#Spam Bot Created in Python
+
+import pyautogui
+import time
+
+text = input("Enter the text you would like to spam: ")
+n = input("Enter the number of times you would like to spam the text:")
+x = int(input("Enter the number of times you would like to spam the text:"))
+t = int(input("Enter the time interval between each spam in seconds:"))
+time.sleep(t)
+
+for i in range(x):
+    pyautogui.typewrite(text)
+    pyautogui.press("Enter")

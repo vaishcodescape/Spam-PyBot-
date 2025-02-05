cd322523757ad7064d8feb898e96bbabd888abbc
Aditya Vaish <adityavaish846@gmail.com>
2025-02-05 18:54:41 +0530

    Update spam-pybot.py

--- spam-pybot.py
+++ spam-pybot.py
@@ -1,13 +1,13 @@
 #Spam Bot Created in Python
+
 import pyautogui
 import time
 
 text = input("Enter the text you would like to spam: ")
-n = input("Enter the number of times you would like to spam the text:")
-x = int(input("Enter the number of times you would like to spam the text:"))
+num = int(input("Enter the number of times you would like to spam the text:"))
 t = int(input("Enter the time interval between each spam in seconds:"))
-time.sleep(t)
+time.sleep(time)
 
-for i in range(x):
+for i in range(num):
     pyautogui.typewrite(text)
     pyautogui.press("Enter")
